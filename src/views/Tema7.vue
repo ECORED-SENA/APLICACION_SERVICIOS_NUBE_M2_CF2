d136d7efc4a072d0e7529a99accc7b8f708d4c9c
elrilacho <80546125+elrilacho@users.noreply.github.com>
2021-04-23 09:04:31 -0500

    Creacion de temas restantes

---
+++ src/views/Tema7.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+.curso-main-container.pb-3
+  BannerInterno
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5
+    .titulo-principal
+      .titulo-principal__numero
+        .h3 7
+      .h3 Programación modular
+
+</template>
+
+<script>
+export default {
+  name: 'Tema7',
+  data: () => ({
+    // variables de vue
+  }),
+}
+</script>
+
+<style lang="sass" scoped></style>
